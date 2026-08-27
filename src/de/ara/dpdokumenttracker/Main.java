cf7674311263446e8f0db0e2eed580eeab9788a3
Arsen Hevorkian <ahevorkian@cdtnue.onmicrosoft.com>
2026-08-27 14:00:18 +0200

    first commit

---
+++ src/de/ara/dpdokumenttracker/Main.java
@@ -0,0 +1,38 @@
+package de.ara.dpdokumenttracker;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.io.IOException;
+
+
+public class Main {
+
+	public static void main(String[] args) 
+			throws IOException, InterruptedException{
+		
+		String url = "https://munich.pasport.org.ua/solutions/e-queue";
+		
+		HttpClient client = HttpClient.newHttpClient();
+		
+		HttpRequest request = HttpRequest.newBuilder()
+				.uri(URI.create(url))
+				.GET()
+				.build();
+		
+		HttpResponse<String> response =
+                client.send(
+                        request,
+                        HttpResponse.BodyHandlers.ofString()
+                );
+
+        String html = response.body();
+
+        System.out.println(html);
+
+		
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Dp_dokument_tracker {
+	requires java.net.http;
+}
