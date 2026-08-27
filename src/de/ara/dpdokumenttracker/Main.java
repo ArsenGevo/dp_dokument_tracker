a0be387a495d5643c20435abf1dcc5d8e3955ae7
Arsen Hevorkian <ahevorkian@cdtnue.onmicrosoft.com>
2026-08-27 15:54:28 +0200

    Add appointment status check and error handling

--- src/de/ara/dpdokumenttracker/Main.java
+++ src/de/ara/dpdokumenttracker/Main.java
@@ -16,7 +16,7 @@ public class Main {
         ERROR
     }
 
-	public static void main(String[] args) throws IOException, InterruptedException {
+	public static void main(String[] args) {
 		
 	    AppointmentStatus status;
 
